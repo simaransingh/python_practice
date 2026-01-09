cde8169667b5e01d3fe5b2cb57b2541e0aed669d
SimranPythonDev <simransingh96964224@gmail.com>
2026-01-09 09:13:05 -0800

    added Day 1 Python basics

---
+++ day1.py
@@ -0,0 +1,17 @@
+print("python is working")
+print("hello simran")
+print("my placement preparation starts today")
+name= input("enter your name :")
+print("welcome",name)
+name = "simran"
+age =20
+branch = "cse"
+print(name)
+print(age)
+print(branch)
+name = input("enter your name :")
+age = input("enter your age :")
+print("hello", name)
+print("your age",age, "years old")
+city = input("enter your city :")
+print("you live in",city)
